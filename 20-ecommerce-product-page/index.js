2f330ce52d201c8ab327a6dbc6a1f583ee732559
Favour Ohenhen <fohenhen7@gmail.com>
2026-09-23 12:36:27 +0100

    feat: add cart badge functionality and update styles for cart icon

--- 20-ecommerce-product-page/index.js
+++ 20-ecommerce-product-page/index.js
@@ -1,6 +1,10 @@
 let countEl = document.getElementById('count-el')
 // console.log(countEl)
+let countBadge = document.getElementById("badge")
 
+
+const mainImg = document.getElementById("main-img")
+console.log(mainImg)
 let count = 0
 
 function increment(){
@@ -19,5 +23,25 @@ countEl.innerText = count
 }
 
 function add(){
+    countBadge.textContent = count
     console.log(count)
+}
+
+
+const imagesrc1 = "./images/image-product-1.jpg"
+const imagesrc2 = "./images/image-product-2.jpg"
+const imagesrc3 = "./images/image-product-3.jpg"
+const imagesrc4 = "./images/image-product-4.jpg"
+
+function imgscr(){
+    if (number === 1){
+        mainImg = imagesrc1;
+    }else if(number === 2){
+        mainImg = imagesrc2
+    }
+    else if (number === 3){
+        mainImg = imagesrc3
+    }else (number === 4);{
+        mainImg = imagesrc4
+    }
 }
