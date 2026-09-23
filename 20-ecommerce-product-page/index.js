db8e93b700f2d7ae5e85060081d9af846d384e24
Favour Ohenhen <fohenhen7@gmail.com>
2026-09-23 15:59:17 +0100

    feat: update product gallery structure and styles for improved responsiveness for different screen sizes

--- 20-ecommerce-product-page/index.js
+++ 20-ecommerce-product-page/index.js
@@ -3,26 +3,26 @@ let countEl = document.getElementById('count-el')
 let countBadge = document.getElementById("badge")
 
 
-const mainImg = document.getElementById("main-img")
+const mainImg = document.querySelector("#main-img")
 console.log(mainImg)
 let count = 0
 
-function increment(){
-count += 1
-countEl.innerText = count
+function increment() {
+    count += 1
+    countEl.innerText = count
 }
 
-function decrease(){
-if (count >=1){
-   count -= 1
-}else{
-    count = 0
-}
-countEl.innerText = count
+function decrease() {
+    if (count >= 1) {
+        count -= 1
+    } else {
+        count = 0
+    }
+    countEl.innerText = count
 
 }
 
-function add(){
+function add() {
     countBadge.textContent = count
     console.log(count)
 }
@@ -33,15 +33,17 @@ const imagesrc2 = "./images/image-product-2.jpg"
 const imagesrc3 = "./images/image-product-3.jpg"
 const imagesrc4 = "./images/image-product-4.jpg"
 
-function imgscr(){
-    if (number === 1){
-        mainImg = imagesrc1;
-    }else if(number === 2){
-        mainImg = imagesrc2
-    }
-    else if (number === 3){
-        mainImg = imagesrc3
-    }else (number === 4);{
-        mainImg = imagesrc4
-    }
+function imgscr() {
+    // if (number === 1) {
+    //     mainImg = imagesrc1;
+    // } else if (number === 2) {
+    //     mainImg = imagesrc2
+    // }
+    // else if (number === 3) {
+    //     mainImg = imagesrc3
+    // } else (number === 4); {
+    //     mainImg = imagesrc4
+    // }
+    count += 2
+    console.log(count)
 }
